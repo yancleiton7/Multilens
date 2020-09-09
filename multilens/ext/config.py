333e0fe24e568dac5f3366e905d18f64bea6287e
uescarvalho <uesleicdoliveira@gmail.com>
2020-09-09 13:26:54 -0300

    Deploy para o servidor da Heroku

--- multilens/ext/config.py
+++ multilens/ext/config.py
@@ -1,6 +1,8 @@
+import os
 from dynaconf import FlaskDynaconf
 
 
 def init_app(app):
     FlaskDynaconf(app)
+    app.config["SECRET_KEY"] = os.getenv(r"\xc2Q?\x9cF\xe6M[A\xda\xc5\xe7\xb2\x1e<\xa4", "multilens")
     app.config.load_extensions("EXTENSIONS")
