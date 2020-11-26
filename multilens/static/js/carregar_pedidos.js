ca0d7b93c49eb4f7e07ee0622fe4d469bd0bad37
Galdino17 <Galdino17@users.noreply.github.com>
2020-11-26 11:56:02 -0300

    Adicinado funções e colunas novas
    Para carregar os pedidos itens adicionado valor unitario e valor total, e atrelado mulplicação na alteração dos campos valor unitario e quantidade.

--- multilens/static/js/carregar_pedidos.js
+++ multilens/static/js/carregar_pedidos.js
@@ -3,7 +3,7 @@ $(window).load(function() {
     var quantidade_linhas = $("[name=load]").attr('id')
     var id_pedido = $("[name=pedido_id]").attr('id')
 
-    function retornar_clientes(id) {
+    function retornar_pedido(id) {
         var resposta
         
         $.ajax({
@@ -23,7 +23,7 @@ $(window).load(function() {
 
 
    
-    var pedido = retornar_clientes(id_pedido)
+    var pedido = retornar_pedido(id_pedido)
 
 
     for (i = 0; i < quantidade_linhas; i++) {
@@ -38,12 +38,16 @@ $(window).load(function() {
             $('#produto').attr({'name': "profduto", 'id':"profduto"})
             $('#quantidade').attr({'name': "quanftidade", 'id':"quanftidade"})
             $('#descricao').attr({'name': "descrdicao", 'id':"descrdicao"})
+            $('#valor_unitario').attr({'name': "valor_uncitario", 'id':"valor_uncitario"})
+            $('#valor_total').attr({'name': "valor_totcal", 'id':"valor_totcal"})
         }
         else{
             
         $('#produto').attr({'name': "produto"+i, 'id':"produto"+i})
         $('#quantidade').attr({'name': "quantidade"+i, 'id':"quantidade"+i})
         $('#descricao').attr({'name': "descricao"+i, 'id':"descricao"+i})
+        $('#valor_unitario').attr({'name': "valor_unitario"+i, 'id':"valor_unitario"+i})
+        $('#valor_total').attr({'name': "valor_total"+i, 'id':"valor_total"+i})
         
         }
       }
@@ -52,6 +56,8 @@ $(window).load(function() {
       $('#profduto').attr({'name': "produto", 'id':"produto"})
       $('#quanftidade').attr({'name': "quantidade", 'id':"quantidade"})
       $('#descrdicao').attr({'name': "descricao", 'id':"descricao"})
+      $('#valor_uncitario').attr({'name': "valor_unitario", 'id':"valor_unitario"})
+      $('#valor_totcal').attr({'name': "valor_total", 'id':"valor_total"})
      
       
 
@@ -59,9 +65,38 @@ $(window).load(function() {
 
 $(document).ready(function() {
     
-   
+    
+
+     
+
     
     (function ($) {
+        $.fn.gerar_multiplicacao = function() {
+            if ($(this).attr('id')[0]==='v'){
+                count_valor = $(this).attr('id').split('valor_unitario')[1]
+            } else {
+                count_valor = $(this).attr('id').split('quantidade')[1]
+            }
+            
+
+            if ($('#valor_unitario'+count_valor).val()!='' && $('#quantidade'+count_valor).val()!=''){
+                valor_unitario = parseFloat($('#valor_unitario'+count_valor).val().replace(",", "."))
+                quantidade = parseInt($('#quantidade'+count_valor).val())
+                valor_total = valor_unitario * quantidade
+                reais = Math.trunc(valor_total)
+                centavos = Math.trunc((valor_total-reais)*100)
+                
+    
+                if (centavos===0){centavos="00"}
+                else if (centavos<10){centavos="0"+centavos.toString()}
+    
+                valor_input = reais.toString()+","+centavos
+    
+                $('#valor_total'+count_valor).val(valor_input)
+            }
+
+        }
+
         $.fn.myfunction = function() {
     
         if (count_item<20){
@@ -72,23 +107,36 @@ $(document).ready(function() {
             div_adicinar = JSON.parse(div_adicinar);
             
 
-            novo_nome_produto = 'name="produto'+count_item+'"'
-            novo_nome_quantidade = 'name="quantidade'+count_item+'"'
-            novo_nome_descricao = 'name="descricao'+count_item+'"'
+            novo_nome_produto = '"produto'+count_item+'"'
+            novo_nome_quantidade = '"quantidade'+count_item+'"'
+            novo_nome_descricao = '"descricao'+count_item+'"'
+            novo_nome_valor_unitario = '"valor_unitario'+count_item+'"'
+            novo_nome_valor_total = '"valor_total'+count_item+'"'
 
             
 
-            div_adicinar = div_adicinar.replace('name="produto"', novo_nome_produto)
-            div_adicinar = div_adicinar.replace('name="quantidade"', novo_nome_quantidade)
-            div_adicinar = div_adicinar.replace('name="descricao"', novo_nome_descricao)
+            div_adicinar = div_adicinar.replace('name="produto"', "name="+novo_nome_produto)
+            div_adicinar = div_adicinar.replace('name="quantidade"', "name="+novo_nome_quantidade)
+            div_adicinar = div_adicinar.replace('name="descricao"', "name="+novo_nome_descricao)
+            div_adicinar = div_adicinar.replace('name="valor_unitario"', "name="+novo_nome_valor_unitario)
+            div_adicinar = div_adicinar.replace('name="valor_total"', "name="+novo_nome_valor_total)
+            
+            div_adicinar = div_adicinar.replace('id="produto"', "id="+novo_nome_produto)
+            div_adicinar = div_adicinar.replace('id="quantidade"', "id="+novo_nome_quantidade)
+            div_adicinar = div_adicinar.replace('id="descricao"', "id="+novo_nome_descricao)
+            div_adicinar = div_adicinar.replace('id="valor_unitario"', "id="+novo_nome_valor_unitario)
+            div_adicinar = div_adicinar.replace('id="valor_total"', "id="+novo_nome_valor_total)
+            
             div_adicinar = div_adicinar.replace('text" value="'+$("[name=quantidade]").val(), 'text" value="')
             div_adicinar = div_adicinar.replace('text" value="'+$("[name=descricao]").val(), 'text" value="')
+            div_adicinar = div_adicinar.replace('text" value="'+$("[name=valor_unitario]").val(), 'text" value="')
+            div_adicinar = div_adicinar.replace('text" value="'+$("[name=valor_total]").val(), 'text" value="')
             div_adicinar = div_adicinar.replace('<div class="field is-grouped">', '<div class="field is-grouped" id="item-'+count_item+'">')
-           
+            console.log(div_adicinar)
             
               
             $('#form_principal').append(div_adicinar); //add input box
-    
+
                count_item+=1
                
                
@@ -117,8 +165,21 @@ $(document).ready(function() {
         }
         
     });
-        
-  
 
+    $('#form_principal').delegate('[id^=valor_unitario]','keyup', function (e) { 
+        $(this).gerar_multiplicacao()
+    });
+
+    $('#form_principal').delegate('[id^=quantidade]','keyup', function (e) { 
+        $(this).gerar_multiplicacao()
+    });
+        
+    $($('[id^=valor_unitario]')).keyup(function () {
+        $(this).gerar_multiplicacao()
+    });
+
+    $($('[id^=quantidade]')).keyup(function () {
+        $(this).gerar_multiplicacao()
+    });
 
 })
